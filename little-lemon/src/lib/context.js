d0e3f7d799bf292330367c4b0e72ba5a06ae4203
Belloshehu <belloshehu1@gmail.com>
2023-01-21 12:30:57 +0100

    Create App context provider

---
+++ little-lemon/src/lib/context.js
@@ -0,0 +1,21 @@
+import {createContext, useContext, useState} from "react";
+
+const AppContext = createContext(null);
+
+const AppContextProvider = ({children}) =>{
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <AppContext.Provider value={{
+            isOpen,
+            openNav: ()=>{ setIsOpen(true)},
+            closeNav: ()=>{ setIsOpen(false)},
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
+
+export default AppContextProvider;
