6b7cb0e61d0964b0affadba6959f27f359d2cf40
Belloshehu <belloshehu1@gmail.com>
2023-01-21 17:10:44 +0100

    Finish Hero section

---
+++ little-lemon/src/components/Hero/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './index.css'
+import heroImage from '../../assets/restauranfood.jpg'
+import { Link } from 'react-router-dom'
+
+const Hero = () => {
+  return (
+    <section className='hero-section'>
+        <div className='hero-container'>
+            <div className='title-container'>
+                <h1 className='title'>Little Lemon</h1>
+                <h4 className='sub-title'>Chicago</h4>
+            </div>
+
+            <p className='paragraph'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Distinctio laboriosam esse itaque eum illum explicabo qui corrupti dolores animi facere.
+                Quasi harum eum at deleniti quod dolorem. Quae, odio cum.
+            </p>
+
+            <Link to='/order' className='btn bg-yellow bold'>Get started</Link>
+        </div>
+        
+        <img src={heroImage} alt='restaurant-photo' className='hero-image'/>
+    </section>
+  )
+}
+
+export default Hero
